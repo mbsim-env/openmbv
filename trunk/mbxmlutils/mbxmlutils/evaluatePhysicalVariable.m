6b1c05fe64aeb711d6cb8fec3eb16c73b492231d
mafriedrich <mafriedrich@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2009-05-15 15:42:35 +0000

    Fixed octave script for octave 3

--- trunk/mbxmlutils/mbxmlutils/evaluatePhysicalVariable.m
+++ trunk/mbxmlutils/mbxmlutils/evaluatePhysicalVariable.m
@@ -7,13 +7,15 @@ else
 end
 
 myargv=argv;
-FILENAME=myargv{1};
+INFILENAME=myargv{1};
+OUTFILENAME=myargv{2};
 % open file
-f=fopen(FILENAME,'rt');
+fin=fopen(INFILENAME,'rt');
+fout=fopen(OUTFILENAME,'wt');
 nr=0;
 while(1)
   % read on line of file
-  line=fgetl(f);
+  line=fgetl(fin);
   nr=nr+1;
   if octave2
     fid=fopen('.line', 'w'); fprintf(fid, '%s', line); fclose(fid);
@@ -24,7 +26,7 @@ while(1)
   end
   if size(match)(1)==0 | size(match)(2)==0
     % if nothing to evaluate, print line
-    printf('%s\n',line);
+    fprintf(fout,'%s\n',line);
   else
     % get expression to evaluate (and pre, post-expression)
     if octave2
@@ -44,37 +46,37 @@ while(1)
     if strcmp(type,'SCALAR')==1 || strcmp(type,'SCALARINTEGER')==1
       % if scalar, check size
       if sum(size(val)!=[1,1])
-        printf('%s:%d: ERROR: Scalar expected, but got value of size %dx%d\n',FILENAME,nr,size(val));
+        printf('%s:%d: ERROR: Scalar expected, but got value of size %dx%d\n',INFILENAME,nr,size(val));
         exit;
       end
       % if scalarinteger, check for integer
       if strcmp(type,'SCALARINTEGER')==1 && round(val)!=val
-        printf('%s:%d: ERROR: Scalar integer expected, but got %e\n',FILENAME,nr,val);
+        printf('%s:%d: ERROR: Scalar integer expected, but got %e\n',INFILENAME,nr,val);
         exit;
       end
       % if scalar print evaluated line
       if strcmp(type,'SCALARINTEGER')!=1
-        printf('%s%.15e%s\n', preexp, val, postexp);
+        fprintf(fout,'%s%.15e%s\n', preexp, val, postexp);
       else
-        printf('%s%d%s\n', preexp, val, postexp);
+        fprintf(fout,'%s%d%s\n', preexp, val, postexp);
       end
     else
       % if vector, check size
       if strcmp(type,'VECTOR')==1 && size(val,2)!=1
-        printf('%s:%d: ERROR: Column vector expected, but got value of size %dx%d\n',FILENAME,nr,size(val));
+        printf('%s:%d: ERROR: Column vector expected, but got value of size %dx%d\n',INFILENAME,nr,size(val));
         exit;
       end
       % if vector or matrix print evaluated line
-      printf('%s[ ', preexp);
+      fprintf(fout,'%s[ ', preexp);
       for r=1:size(val,1)
         for c=1:size(val,2)
-          printf('%.15e',val(r,c));
-          if c!=size(val,2), printf(' , '); end
+          fprintf(fout,'%.15e',val(r,c));
+          if c!=size(val,2), fprintf(fout,' , '); end
         end
-        if r!=size(val,1), printf(' ; '); end
+        if r!=size(val,1), fprintf(fout,' ; '); end
       end
-      printf(' ]%s\n', postexp);
+      fprintf(fout,' ]%s\n', postexp);
     end
   end
-  if feof(f), break; end
+  if feof(fin), break; end
 end
