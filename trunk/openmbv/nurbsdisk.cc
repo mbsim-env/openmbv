0afab9936283724b29f88ebc1d59c815c32f8de5
thschindler <thschindler@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2009-05-20 08:37:14 +0000

    nurbsdisk first version

--- trunk/openmbv/nurbsdisk.cc
+++ trunk/openmbv/nurbsdisk.cc
@@ -18,18 +18,17 @@
    */
 
 #include "config.h"
-#include "spineextrusion.h"
+#include "nurbsdisk.h"
 #include "mainwindow.h"
 #include <Inventor/nodes/SoMaterial.h>
 
 using namespace std;
 
-SpineExtrusion::SpineExtrusion(TiXmlElement *element, H5::Group *h5Parent, QTreeWidgetItem *parentItem, SoGroup *soParent) : Body(element, h5Parent, parentItem, soParent), numberOfSpinePoints(0) {
+NurbsDisk::NurbsDisk(TiXmlElement *element, H5::Group *h5Parent, QTreeWidgetItem *parentItem, SoGroup *soParent) : Body(element, h5Parent, parentItem, soParent) {
   //h5 dataset
   h5Data=new H5::VectorSerie<double>;
   if(h5Group) {
     h5Data->open(*h5Group, "data");
-    numberOfSpinePoints = int((h5Data->getRow(1).size()-1)/4);
     int rows=h5Data->getRows();
     double dt;
     if(rows>=2) dt=h5Data->getRow(1)[0]-h5Data->getRow(0)[0]; else dt=0;
@@ -59,61 +58,61 @@ SpineExtrusion::SpineExtrusion(TiXmlElement *element, H5::Group *h5Parent, QTree
   mat->specularColor.setHSVValue((1-color)*2/3,0.7,1);
 
   // body
-  extrusion=new SoVRMLExtrusion;
-  soSep->addChild(extrusion);
-  twistAxis = new SbVec3f(0.,1.,0.);
-
-  // scale
-  extrusion->scale.setNum(numberOfSpinePoints);
-  SbVec2f *sc = extrusion->scale.startEditing();
-  for(int i=0;i<numberOfSpinePoints;i++) sc[i] = SbVec2f(scaleValue,scaleValue); // first x-scale / second z-scale
-  extrusion->scale.finishEditing();
-  extrusion->scale.setDefault(FALSE);
-
-  // cross section
-  extrusion->crossSection.setNum(contour.size()+1);
-  SbVec2f *cs = extrusion->crossSection.startEditing();
-  for(int i=0;i<contour.size();i++) cs[i] = SbVec2f(contour[i][0], contour[i][1]); // clockwise in local coordinate system
-  cs[contour.size()] =  SbVec2f(contour[0][0], contour[0][1]); // closed cross section
-  extrusion->crossSection.finishEditing();
-  extrusion->crossSection.setDefault(FALSE);
-
-  // additional flags
-  extrusion->solid=TRUE; // backface culling
-  extrusion->convex=TRUE; // only convex polygons included in visualisation
-  extrusion->ccw=TRUE; // vertex ordering counterclockwise?
-  extrusion->beginCap=FALSE; // front side at begin of the spine
-  extrusion->endCap=FALSE; // front side at end of the spine
-  extrusion->creaseAngle=0.3; // angle below which surface normals are drawn smooth
+//  extrusion=new SoVRMLExtrusion;
+//  soSep->addChild(extrusion);
+//  twistAxis = new SbVec3f(0.,1.,0.);
+//
+//  // scale
+//  extrusion->scale.setNum(numberOfSpinePoints);
+//  SbVec2f *sc = extrusion->scale.startEditing();
+//  for(int i=0;i<numberOfSpinePoints;i++) sc[i] = SbVec2f(scaleValue,scaleValue); // first x-scale / second z-scale
+//  extrusion->scale.finishEditing();
+//  extrusion->scale.setDefault(FALSE);
+//
+//  // cross section
+//  extrusion->crossSection.setNum(contour.size()+1);
+//  SbVec2f *cs = extrusion->crossSection.startEditing();
+//  for(int i=0;i<contour.size();i++) cs[i] = SbVec2f(contour[i][0], contour[i][1]); // clockwise in local coordinate system
+//  cs[contour.size()] =  SbVec2f(contour[0][0], contour[0][1]); // closed cross section
+//  extrusion->crossSection.finishEditing();
+//  extrusion->crossSection.setDefault(FALSE);
+//
+//  // additional flags
+//  extrusion->solid=TRUE; // backface culling
+//  extrusion->convex=TRUE; // only convex polygons included in visualisation
+//  extrusion->ccw=TRUE; // vertex ordering counterclockwise?
+//  extrusion->beginCap=FALSE; // front side at begin of the spine
+//  extrusion->endCap=FALSE; // front side at end of the spine
+//  extrusion->creaseAngle=0.3; // angle below which surface normals are drawn smooth
 }
 
-QString SpineExtrusion::getInfo() {
+QString NurbsDisk::getInfo() {
   return Body::getInfo();
 }
 
-double SpineExtrusion::update() {
+double NurbsDisk::update() {
   if(h5Group==0) return 0;
   // read from hdf5
   int frame=MainWindow::getInstance()->getFrame()->getValue();
   std::vector<double> data=h5Data->getRow(frame);
 
-  // set spine
-  extrusion->spine.setNum(numberOfSpinePoints);
-  SbVec3f *sp = extrusion->spine.startEditing();
-  for(int i=0;i<numberOfSpinePoints;i++) {
-    sp[i] = SbVec3f(data[4*i+1],data[4*i+2],data[4*i+3]);
-  }
-  extrusion->spine.finishEditing();
-  extrusion->spine.setDefault(FALSE);
-
-  // set twist
-  extrusion->orientation.setNum(numberOfSpinePoints);
-  SbRotation *tw = extrusion->orientation.startEditing();
-  for(int i=0;i<numberOfSpinePoints;i++) {
-    tw[i] = SbRotation(*twistAxis,data[4*i+4]);
-  }
-  extrusion->orientation.finishEditing();
-  extrusion->orientation.setDefault(FALSE);
+//  // set spine
+//  extrusion->spine.setNum(numberOfSpinePoints);
+//  SbVec3f *sp = extrusion->spine.startEditing();
+//  for(int i=0;i<numberOfSpinePoints;i++) {
+//    sp[i] = SbVec3f(data[4*i+1],data[4*i+2],data[4*i+3]);
+//  }
+//  extrusion->spine.finishEditing();
+//  extrusion->spine.setDefault(FALSE);
+//
+//  // set twist
+//  extrusion->orientation.setNum(numberOfSpinePoints);
+//  SbRotation *tw = extrusion->orientation.startEditing();
+//  for(int i=0;i<numberOfSpinePoints;i++) {
+//    tw[i] = SbRotation(*twistAxis,data[4*i+4]);
+//  }
+//  extrusion->orientation.finishEditing();
+//  extrusion->orientation.setDefault(FALSE);
 
   return data[0];
 }
