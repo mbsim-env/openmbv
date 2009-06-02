4efea29f6909fc01a792a82022978c2ae4615d19
mafriedrich <mafriedrich@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2009-06-02 10:21:12 +0000

    fixed regexp to work with more octave 3 versions

--- trunk/mbxmlutils/mbxmlutils/evaluate.m
+++ trunk/mbxmlutils/mbxmlutils/evaluate.m
@@ -10,22 +10,22 @@ while(1)
 
   % search ATTR
   while 1
-    [S,bb,cc,dd,T]=regexp(line, '@ATTRB@(.+?)@ATTRE@');
+    [S,bb,cc,dd,T]=regexp(line, '@ATTRB@([^@]+)@ATTRE@');
     if length(S)==0, break; end
     attr=T{1}{1};
     % subst all {.*?}
     while 1
-      [S,bb,cc,dd,T]=regexp(attr, '{(.+?)}');
+      [S,bb,cc,dd,T]=regexp(attr, '\{([^\}]+)\}');
       if length(S)==0, break; end
       rep=eval(T{1}{1});
       if round(rep)!=rep, error([T{1}{1} ' dose not evaluate to a integer in attribute!']); end
-      attr=regexprep(attr, '{.+?}', sprintf('%d',rep), 'once');
+      attr=regexprep(attr, '\{[^\}]+\}', sprintf('%d',rep), 'once');
     end
-    line=regexprep(line, '@ATTRB@.+?@ATTRE@', sprintf('%s',attr), 'once');
+    line=regexprep(line, '@ATTRB@[^@]+@ATTRE@', sprintf('%s',attr), 'once');
   end
   % search TEXT
   while 1
-    [S,bb,cc,dd,T]=regexp(line, '@TEXTB@(.+?)@TEXTE@');
+    [S,bb,cc,dd,T]=regexp(line, '@TEXTB@([^@]+)@TEXTE@');
     if length(S)==0, break; end
     val=eval(T{1}{1});
     if size(val,1)==1 && size(val,2)==1
@@ -41,7 +41,7 @@ while(1)
       end
       text=[text sprintf(']')];
     end
-    line=regexprep(line, '@TEXTB@.+?@TEXTE@', sprintf('%s',text), 'once');
+    line=regexprep(line, '@TEXTB@[^@]+@TEXTE@', sprintf('%s',text), 'once');
   end
 
   fprintf(fout,'%s\n', line);
