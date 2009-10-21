2f59924d7f8c0f3e81b3d56845feda356de3ffdd
mafriedrich <mafriedrich@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2009-10-21 08:09:47 +0000

    Release 3.0.0

--- mbxmlutils/mbxmlutils/check/runcheck.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-../mbxmlutilspp parameter.xml mygrp.ombv.xml http://openmbv.berlios.de/OpenMBV /home/markus/project/MBSim/local/share/openmbv/schema/openmbv.xsd /dev/stderr
--- mbxmlutils/mbxmlutils/octave/rotateAboutZ.m
+++
@@ -1,4 +0,0 @@
-function A=rotateAboutZ(phi)
-  A=[cos(phi),-sin(phi),0;
-     sin(phi),cos(phi),0;
-     0,0,1];
