0445190170f3280e5c0a2a0f00beb036000374a9
mafriedrich <mafriedrich@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2010-01-22 16:27:02 +0000

    Added a IndexedTesselationFace class for use in the iv-Format Some cleanup

---
+++ trunk/openmbv/openmbv/IndexedTesselationFace.h
@@ -0,0 +1,31 @@
+#ifndef _INDEXEDTESSELATIONFACE_H_
+#define _INDEXEDTESSELATIONFACE_H_
+
+#include <Inventor/nodes/SoGroup.h>
+#include <Inventor/fields/SoMFInt32.h>
+#include <Inventor/fields/SoMFVec3d.h>
+#include <Inventor/fields/SoSFEnum.h>
+#include <Inventor/sensors/SoFieldSensor.h>
+
+class IndexedTesselationFace : public SoGroup {
+ SO_NODE_HEADER(IndexedTesselationFace);
+ public:
+   SoSFEnum windingRule;
+   SoMFVec3d coordinate;
+   SoMFInt32 coordIndex;
+
+   static void initClass();
+   IndexedTesselationFace();
+   IndexedTesselationFace(int numChilderen);
+
+ protected:
+   enum WindingRule { ODD, NONZERO, POSITIVE, NEGATIVE, ABS_GEQ_TWO };
+   static void changedCB(void *data, SoSensor*);
+   friend class Body;
+
+ private:
+   virtual ~IndexedTesselationFace();
+   void constructor();
+};
+
+#endif
