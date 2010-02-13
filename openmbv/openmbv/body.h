11b378952ca74e65c6a36888ae2293352c42925c
mafriedrich <mafriedrich@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2010-02-13 15:45:38 +0000

    Deleted ObjBody, use IvBody till now!

--- openmbv/openmbv/body.h
+++ openmbv/openmbv/body.h
@@ -62,38 +62,6 @@ class Body : public Object {
 
   // FROM NOW ONLY CONVENIENCE FUNCTIONS FOLLOW !!!
   protected:
-    // combine and calcaulate vertex/normal
-    static double eps; // convenience
-    // compares an Vertex like an alphanumeric string
-    class SbVec3fHash { // convenience
-      public:
-        bool operator()(const SbVec3f& v1, const SbVec3f& v2) const;
-    };
-    // returns newvv with deleted duplicated vertices from vv;
-    // also return newvi the a list of new indixies
-    // complexibility: n*log(n)
-    // v: IN vector of vertices
-    // newv: OUT vector of new vertices
-    // newvi: OUT vector of new indcies
-    static void combine(const SoMFVec3f& v, SoMFVec3f& newv, SoMFInt32& newvi); // convenience
-    // substutute the indixies in fv with the new indixes newvi
-    // complexibility: n
-    // fvi: IN/OUT vector for face indicies
-    // newvi: IN vector of new indcies
-    static void convertIndex(SoMFInt32& fvi, const SoMFInt32& newvi); // convenience
-    // cal normals
-    struct XXX { // convenience
-      int ni1, ni2;
-    };
-    struct TwoIndex { // convenience
-      int vi1, vi2;
-    };
-    class TwoIndexHash { // convenience
-      public:
-        bool operator()(const TwoIndex& l1, const TwoIndex& l2) const;
-    };
-    static void computeNormals(const SoMFInt32& fvi, const SoMFVec3f &v, SoMFInt32& fni, SoMFVec3f& n, SoMFInt32& oli, double smoothBarrier); // convenience
-
     // tess // convenience
     static GLUtesselator *tess; // convenience
     static GLenum tessType; // convenience
