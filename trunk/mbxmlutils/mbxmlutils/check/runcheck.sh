b15a150a1888cadc5357659266713c361a3f5b51
mafriedrich <mafriedrich@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2011-03-19 21:25:18 +0000

    Added openmbv.xsd to svn and update the "check" to be able to run with "make check" (Simply run "make check" to test the mbxmlutilspp program; non expert use)

--- trunk/mbxmlutils/mbxmlutils/check/runcheck.sh
+++ trunk/mbxmlutils/mbxmlutils/check/runcheck.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-../mbxmlutilspp parameter.xml mygrp.ombv.xml /home/markus/project/local/share/mbxmlutils/schema/http___openmbv_berlios_de_OpenMBV/openmbv.xsd 
+../mbxmlutilspp parameter.xml mygrp.ombv.xml http___openmbv_berlios_de_OpenMBV/openmbv.xsd 
