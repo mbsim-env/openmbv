54613692f8b355e0047f5bb3b255c3a958797f7c
mafriedrich <mafriedrich@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2011-03-19 20:06:49 +0000

    fixed check script for mbxmltuils (NOTE: this script must manually be updated to the correct path, ...; a schema must already be installed, ...; => currentl yonly for experts use)

--- mbxmlutils/mbxmlutils/check/runcheck.sh
+++ mbxmlutils/mbxmlutils/check/runcheck.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-../mbxmlutilspp parameter.xml mygrp.ombv.xml http://openmbv.berlios.de/OpenMBV /home/markus/project/MBSim/local/share/openmbv/schema/openmbv.xsd /dev/stderr
+../mbxmlutilspp parameter.xml mygrp.ombv.xml /home/markus/project/local/share/mbxmlutils/schema/http___openmbv_berlios_de_OpenMBV/openmbv.xsd 
