88943d828bb64910f9983f1960e1281f5340065a
friedrich.at.gc@gmail.com <friedrich.at.gc@gmail.com@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2012-11-25 18:21:54 +0000

    Added missing files

---
+++ mbxmlutils/mbxmlutilstinyxml/getinstallpath.h
@@ -0,0 +1,12 @@
+#ifndef _MBXMLUTILS_GETINSTALLPATH_H_
+#define _MBXMLUTILS_GETINSTALLPATH_H_
+
+#include <string>
+
+namespace MBXMLUtils {
+
+std::string getInstallPath();
+
+} // end namespace MBXMLUtils
+
+#endif
