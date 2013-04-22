e5172655d8414e1e7774b1b6af935621fcdbf82f
friedrich.at.gc <friedrich.at.gc@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2013-04-22 20:45:16 +0000

    The SWIG java interface uses now the java double[] type for std::vector<double> in c++: this should enable the java interface seamless in Matlab.

--- openmbvcppinterface/swig/java/javatest.java
+++ openmbvcppinterface/swig/java/javatest.java
@@ -26,22 +26,11 @@ public class javatest {
     String ret=cube.getName(); System.out.println(ret);
     boolean b=cube.getReferenceFrame(); System.out.println(b);
     double d=cube.getLength(); System.out.println(d);
-    VectorDouble dv=cuboid.getLength(); System.out.println(dv.get(0)); System.out.println(dv.get(1)); System.out.println(dv.get(2));
+    double[] dv=cuboid.getLength(); System.out.println(dv[0]); System.out.println(dv[1]); System.out.println(dv[2]);
 
-    VectorDouble vec=new VectorDouble();
-    vec.add(5.6); vec.add(9.7); vec.add(3.5);
-    cuboid.setLength(vec);
-    dv=cuboid.getLength(); System.out.println(dv.get(0)); System.out.println(dv.get(1)); System.out.println(dv.get(2));
+    cuboid.setLength(new double[]{5.6, 9.7, 3.5});
+    dv=cuboid.getLength(); System.out.println(dv[0]); System.out.println(dv[1]); System.out.println(dv[2]);
     
-    VectorDouble append=new VectorDouble();
-    append.add(0.4);
-    append.add(1);
-    append.add(2);
-    append.add(3);
-    append.add(4);
-    append.add(5);
-    append.add(6);
-    append.add(0.25);
-    cube.append(append);
+    cube.append(new double[]{0.4, 1, 2, 3, 4, 5, 6, 0.25});
   }
 }
