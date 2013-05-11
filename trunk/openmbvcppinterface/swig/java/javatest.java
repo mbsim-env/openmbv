b00bb7ddcb9bf6e724af8f8c236d9d9e7a748a3f
friedrich.at.gc <friedrich.at.gc@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2013-05-11 12:45:44 +0000

    Improved swig examples: now including subroutines (check for correct memory management).

--- trunk/openmbvcppinterface/swig/java/javatest.java
+++ trunk/openmbvcppinterface/swig/java/javatest.java
@@ -4,10 +4,30 @@ import de.berlios.openmbv.OpenMBV.*;
 
 public class javatest {
 
+  // main program
   public static void main(String argv[]) {
+    // create main group
     Group group=new Group();
     group.setName("MBS");
+
+    // add some objects to group in this subroutine and return a added cube
+    Cube cube=createMyGroup(group);
     
+    // create H5 and xml file
+    group.setFileName("MBS_outfile.ombv.xml");
+    group.write(true, true);
+    
+    // some action on the returned cube
+    String ret=cube.getName(); System.out.println(ret);
+    boolean b=cube.getReferenceFrame(); System.out.println(b);
+    double d=cube.getLength(); System.out.println(d);
+    cube.append(new double[]{0.4, 1, 2, 3, 4, 5, 6, 0.25});
+  }
+
+
+  // a subroutine
+  public static Cube createMyGroup(Group g) {
+    // create a cube
     Cube cube=new Cube();
     cube.setName("Box1");
     cube.setReferenceFrame(true);
@@ -15,25 +35,22 @@ public class javatest {
     //cube.setLength(new ScalarParameter(1.235)); ScalarParameter is not supported by java
     //cube.setLength(new ScalarParameter("p1", 1.236)); ScalarParameter is not supported by java
     
+    // create a cuboid
     Cuboid cuboid=new Cuboid();
     cuboid.setName("Box2");
     cuboid.setReferenceFrame(true);
     cuboid.setLength(1.234, 3, 4);
     
-    group.addObject(cube);
-    group.addObject(cuboid);
+    // add cube and cuboid to group
+    g.addObject(cube);
+    g.addObject(cuboid);
     
-    group.setFileName("MBS_outfile.ombv.xml");
-    group.write(true, true);
-    
-    String ret=cube.getName(); System.out.println(ret);
-    boolean b=cube.getReferenceFrame(); System.out.println(b);
-    double d=cube.getLength(); System.out.println(d);
+    // some actions on cuboid
     double[] dv=cuboid.getLength(); System.out.println(dv[0]); System.out.println(dv[1]); System.out.println(dv[2]);
-
     cuboid.setLength(new double[]{5.6, 9.7, 3.5});
     dv=cuboid.getLength(); System.out.println(dv[0]); System.out.println(dv[1]); System.out.println(dv[2]);
-    
-    cube.append(new double[]{0.4, 1, 2, 3, 4, 5, 6, 0.25});
+
+    // return the cube
+    return cube;
   }
 }
