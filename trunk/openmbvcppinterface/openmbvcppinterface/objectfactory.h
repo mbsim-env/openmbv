46a13097f37eca386e6a3c7be604d0aea4f4a508
friedrich.at.gc <friedrich.at.gc@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2013-05-26 10:11:37 +0000

    Removed namespace mapping: is now done (very simple) directly in MBSim kernel.

--- trunk/openmbvcppinterface/openmbvcppinterface/objectfactory.h
+++ trunk/openmbvcppinterface/openmbvcppinterface/objectfactory.h
@@ -7,13 +7,8 @@
 namespace OpenMBV {
 
 class ObjectFactory {
-  protected:
-    typedef std::pair<std::string, std::string> P_NSPRE;
-    typedef std::pair<double, P_NSPRE> P_PRINSPRE;
-    typedef std::multimap<double, P_NSPRE> MM_PRINSPRE;
   public:
     static Object* createObject(MBXMLUtils::TiXmlElement *element);
-    static MM_PRINSPRE& getPriorityNamespacePrefix();
 };
 
 }
