be353059d9d5d778cb441a5b60b324ff9306f516
martin.o.foerg@googlemail.com <martin.o.foerg@googlemail.com@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2014-02-13 10:27:46 +0000

    Tagged mbxmlutils version 1.2.0

---
+++ release-1.2.0/mbxmlutils/octave/cardan.m
@@ -0,0 +1,24 @@
+function A=cardan(varargin)
+
+  nargs=length(varargin);
+  if nargs==3
+    alpha=varargin{1};
+    beta=varargin{2};
+    gamma=varargin{3};
+  elseif nargs==1 && (size(varargin{1})==[3,1] || size(varargin{1})==[1,3])
+    alpha=varargin{1}(1);
+    beta=varargin{1}(2);
+    gamma=varargin{1}(3);
+  else
+    error('Must be called with a three scalar arguments or one vector argument of length 3.');
+  end
+
+  A=[cos(beta)*cos(gamma),...
+     -cos(beta)*sin(gamma),...
+     sin(beta);
+     cos(alpha)*sin(gamma)+sin(alpha)*sin(beta)*cos(gamma),...
+     cos(alpha)*cos(gamma)-sin(alpha)*sin(beta)*sin(gamma),...
+     -sin(alpha)*cos(beta);
+     sin(alpha)*sin(gamma)-cos(alpha)*sin(beta)*cos(gamma),...
+     cos(alpha)*sin(beta)*sin(gamma)+sin(alpha)*cos(gamma),...
+     cos(alpha)*cos(beta)];
---
+++ release-1.2.0/mbxmlutils/utils.h
@@ -0,0 +1,69 @@
+#ifndef _MBXMLUTILS_UTILS_H_
+#define _MBXMLUTILS_UTILS_H_
+
+#include <string>
+#include <map>
+#include <vector>
+#include <stack>
+#ifdef HAVE_UNORDERED_MAP
+#  include <unordered_map>
+#else
+#  include <map>
+#  define unordered_map map
+#endif
+#include <octave/oct.h>
+
+#define MBXMLUTILSPVNS "{http://openmbv.berlios.de/MBXMLUtils/physicalvariable}"
+
+namespace MBXMLUtils {
+
+class TiXmlElement;
+
+class OctaveEvaluator {
+  public:
+    enum ValueType {
+      ArbitraryType,
+      ScalarType,
+      VectorType,
+      MatrixType,
+      StringType
+    };
+
+    OctaveEvaluator();
+    void octaveAddParam(const std::string &paramName, const octave_value& value, bool useCache=true);
+    void octaveAddParam(const std::string &paramName, double value, bool useCache=true);
+    void octavePushParams();
+    void octavePopParams();
+    TiXmlElement* octaveEvalRet(std::string str, TiXmlElement *e=NULL, bool useCache=true, std::vector<std::pair<std::string, int> > *arg=NULL);
+    static void checkType(const octave_value& val, ValueType expectedType);
+    static std::string octaveGetRet(ValueType expectedType=ArbitraryType);
+    static double octaveGetDoubleRet();
+    static octave_value& octaveGetOctaveValueRet();
+    void fillParam(TiXmlElement *e, bool useCache=true);
+    void saveAndClearCurrentParam();
+    void restoreCurrentParam();
+
+    static void initialize();
+    static void terminate();
+    static void addPath(const std::string &path);
+
+    void eval(TiXmlElement *e, bool useCache=true);
+    void setUnits(const std::map<std::string, std::string> &units_) { units=units_; }
+
+  protected:
+    void toOctave(TiXmlElement *e, bool useCache=true);
+
+    // map of the current parameters
+    std::map<std::string, octave_value> currentParam, savedCurrentParam;
+    // stack of parameters
+    std::stack<std::map<std::string, octave_value> > paramStack;
+    std::vector<int> currentParamHash;
+
+    std::unordered_map<std::string, octave_value> cache;
+
+    std::map<std::string, std::string> units;
+};
+
+} // end namespace MBXMLUtils
+
+#endif
