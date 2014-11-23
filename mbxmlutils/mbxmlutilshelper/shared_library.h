a77642fbd0ec91273fdc4dc4d2634d08c263a19f
friedrich.at.gc <friedrich.at.gc@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2014-11-23 18:28:31 +0000

    Moved some helper code from mbsimxml to mbxmlutils.

---
+++ mbxmlutils/mbxmlutilshelper/shared_library.h
@@ -0,0 +1,28 @@
+#ifndef _MBXMLUTILS_SHAREDLIBRARY_H_
+#define _MBXMLUTILS_SHAREDLIBRARY_H_
+
+#include <boost/filesystem.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+namespace MBXMLUtils {
+
+class SharedLibrary {
+  public:
+    SharedLibrary(const boost::filesystem::path &file_);
+    SharedLibrary(const SharedLibrary& src);
+    ~SharedLibrary();
+    const boost::filesystem::path file;
+    const boost::posix_time::ptime writeTime;
+    bool operator<(const SharedLibrary& b) const { return file<b.file; }
+  private:
+    void init();
+#ifndef _WIN32
+    void* handle;
+#else
+    HMODULE handle;
+#endif
+};
+
+}
+
+#endif
