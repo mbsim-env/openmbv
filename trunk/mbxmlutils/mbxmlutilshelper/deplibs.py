793492cd99e99b56fddb800fcfd666509971cff0
friedrich.at.gc <friedrich.at.gc@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2015-02-22 14:34:16 +0000

    Minor update/rework.

--- trunk/mbxmlutils/mbxmlutilshelper/deplibs.py
+++ trunk/mbxmlutils/mbxmlutilshelper/deplibs.py
@@ -9,6 +9,16 @@ import distutils.spawn
 import platform
 import glob
 
+if sys.version_info[0]==2: # to unify python 2 and python 3
+  def lru_cache(maxsize=128, typed=False): # no lru_cache for python2, just just a empty decorator
+    def wrapper(func): return func
+    return wrapper
+else:
+  from functools import lru_cache
+
+
+
+@lru_cache(maxsize=None)
 def getWindowsEnvPath(name):
   if platform.system()=="Windows":
     return os.environ[name]
@@ -20,58 +30,48 @@ def getWindowsEnvPath(name):
       ret.append(vwin)
     return ';'.join(ret)
   raise RuntimeError('Unknown platform')
+
 def searchWindowsLibrary(libname, libdir):
-  if searchWindowsLibrary.searchDir==None:
-    searchWindowsLibrary.searchDir=[] # is search in order
-    searchWindowsLibrary.searchDir.append(libdir)
-    searchWindowsLibrary.searchDir.append(getWindowsEnvPath('WINSYSDIR'))
-    searchWindowsLibrary.searchDir.append(getWindowsEnvPath('WINDIR'))
-    searchWindowsLibrary.searchDir.append(os.getcwd())
-    searchWindowsLibrary.searchDir.extend(getWindowsEnvPath('PATH').split(';'))
-  for d in searchWindowsLibrary.searchDir:
+  searchDir=[] # is search in order
+  searchDir.append(libdir)
+  searchDir.append(getWindowsEnvPath('WINSYSDIR'))
+  searchDir.append(getWindowsEnvPath('WINDIR'))
+  searchDir.append(os.getcwd())
+  searchDir.extend(getWindowsEnvPath('PATH').split(';'))
+  for d in searchDir:
     for f in glob.glob(d+'/*'):
       if os.path.basename(f.upper())==libname.upper():
         return f
   raise RuntimeError('Library '+libname+' not found')
-searchWindowsLibrary.searchDir=None
 
+@lru_cache(maxsize=None)
 def getDependencies(filename):
-  try:
-    return getDependencies.result[filename]
-  except KeyError:
-    pass
-  res=[set(), True]
-  getDependencies.result[filename]=res
-  try:
-    content=subprocess.check_output(["objdump", "-a", filename], stderr=open(os.devnull,"w")).decode('utf-8')
-  except subprocess.CalledProcessError:
-    print('WARNING: '+filename+' is not a real library. Skipping.', file=sys.stderr)
-    res[1]=False
-    return res
-  if content.find('file format elf')>=0: # Linux
+  res=set()
+  content=subprocess.check_output(["file", "-L", filename], stderr=open(os.devnull,"w")).decode('utf-8')
+  if re.search('ELF [0-9]+-bit LSB', content)!=None:
     for line in subprocess.check_output(["ldd", filename], stderr=open(os.devnull,"w")).decode('utf-8').split('\n'):
+      match=re.search("^\s*(.*)\s=>\snot found$", line)
+      if match!=None:
+        raise RuntimeError('Library '+match.expand("\\1")+' not found')
       match=re.search("^.*\s=>\s(.*)\s\(0x[0-9a-fA-F]+\)$", line)
       if match!=None:
-        if match.expand("\\1")!="":
-          res[0].add(match.expand("\\1"))
-    res[1]=True
+        res.add(match.expand("\\1"))
     return res
-  elif content.find('file format pei')>=0: # Windows
+  elif re.search('PE[0-9]+ executable', content)!=None:
     for line in subprocess.check_output(["objdump", "-p", filename], stderr=open(os.devnull,"w")).decode('utf-8').split('\n'):
       match=re.search("^\s*DLL Name:\s(.*)$", line)
       if match!=None:
-        res[0].add(searchWindowsLibrary(match.expand("\\1"), os.path.dirname(filename)))
-    res[1]=True
+        res.add(searchWindowsLibrary(match.expand("\\1"), os.path.dirname(filename)))
     return res
-  else:
-    raise RuntimeError('Unknown extension')
-getDependencies.result=dict()
+  raise RuntimeError(filename+' unknown executable format')
 
+@lru_cache(maxsize=None)
 def getDoNotAdd():
   notAdd=set()
-  system=[("equery", ["files", "glibc"]), # Gentoo
-          ("dpkg",   ["-L", "glibc"]),    # debian
-          ("rpm",    ["-ql", "glibc"])]   # rpm
+  system=[
+    ("equery", ["-C", "files", "glibc", "mesa",       "fontconfig",]), # portage
+    ("rpm",    ["-ql",         "glibc", "mesa-libGL", "fontconfig",]), # rpm
+  ]
   for s in system:
     if distutils.spawn.find_executable(s[0]):
       for line in subprocess.check_output([s[0]]+s[1], stderr=open(os.devnull,"w")).decode('utf-8').split('\n'):
@@ -79,28 +79,41 @@ def getDoNotAdd():
       return notAdd
   return notAdd
 
-def walkDependencies(filename, deps):
-  if filename not in deps:
-    res=getDependencies(filename)
-    if res[1]:
+@lru_cache(maxsize=None)
+def relDir(filename):
+  content=subprocess.check_output(["file", "-L", filename], stderr=open(os.devnull,"w")).decode('utf-8')
+  if re.search('ELF [0-9]+-bit LSB', content)!=None:
+    return "lib" # Linux
+  elif re.search('PE[0-9]+ executable', content)!=None:
+    return "bin" # Windows
+  else:
+    raise RuntimeError(filename+' unknwon executable format')
+
+@lru_cache(maxsize=None)
+def depLibs(filename):
+  def walkDependencies(filename, deps):
+    if filename not in deps:
+      res=getDependencies(filename)
       deps.add(filename)
-    for d in res[0]:
-      walkDependencies(d, deps)
+      for d in res:
+        walkDependencies(d, deps)
+  ret=set()
+  walkDependencies(filename, ret)
+  # remove the library itself
+  ret.remove(filename)
+  
+  for n in getDoNotAdd():
+    if n in ret:
+      ret.remove(n)
+  return ret
 
-deps=set()
-walkDependencies(sys.argv[1], deps)
-# remove the library itself
-deps.remove(sys.argv[1])
 
-for n in getDoNotAdd():
-  if n in deps:
-    deps.remove(n)
 
-if os.path.splitext(sys.argv[1])[1]==".dll":
-  reldir="bin"
-else:
-  reldir="lib"
-print('<DependentShFiles>')
-for d in deps:
-  print('  <file reldir="%s" orgdir="%s">%s</file>'%(reldir, os.path.dirname(d), os.path.basename(d)))
-print('</DependentShFiles>')
+# when started as a program
+if __name__=="__main__":
+  deps=depLibs(sys.argv[1])
+  reldir=relDir(sys.argv[1])
+  print('<DependentShFiles>')
+  for d in deps:
+    print('  <file reldir="%s" orgdir="%s">%s</file>'%(reldir, os.path.dirname(d), os.path.basename(d)))
+  print('</DependentShFiles>')
