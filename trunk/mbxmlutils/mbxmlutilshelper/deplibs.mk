aa39f7e171d955628760e336c795471982fcedce
friedrich.at.gc <friedrich.at.gc@853e73f3-7eb9-fbc5-9501-2a6b3cb7cbaa>
2015-03-26 19:40:05 +0000

    Fixed *.deplibs files generation. Do not generate the file in case of errors to enforce regeneration on next run.

--- trunk/mbxmlutils/mbxmlutilshelper/deplibs.mk
+++ trunk/mbxmlutils/mbxmlutilshelper/deplibs.mk
@@ -1,5 +1,6 @@
 # Note: with automake 1.14 we can use %reldir% instead of $(prefix)/share/mbxmlutils/python!
 deplibs.target:
+	set -e; \
 	echo "Create dependency file(s) (only if python is found) for $(lib_LTLIBRARIES)"
 	if which python &> /dev/null; then \
 	  for lib_la in $(lib_LTLIBRARIES); do \
@@ -7,7 +8,8 @@ deplibs.target:
 	    if test -n "$$dlname"; then \
 	      if test $$libdir/$$dlname -nt $$libdir/$$dlname.deplibs; then \
 	        echo "Create dependency files for $$libdir/$$dlname"; \
-	        python $(prefix)/share/mbxmlutils/python/deplibs.py $$libdir/$$dlname > $$libdir/$$dlname.deplibs || exit; \
+	        python $(prefix)/share/mbxmlutils/python/deplibs.py $$libdir/$$dlname > $$libdir/$$dlname.deplibs.tmp; \
+	        mv -f $$libdir/$$dlname.deplibs.tmp $$libdir/$$dlname.deplibs; \
 	      fi; \
 	    fi; \
 	  done; \
