851bc14d7c6062c7eae1b2f0552e32c4a6d71ff1
Markus Friedrich <friedrich.at.gc@googlemail.com>
2018-03-19 22:58:57 +0100

    Revert "Temporarily removed all QWebKit code from openmbv."
    This reverts commit 9bf9b72b3961aa2a4eaf22e135d9b7073a88a8d9.

--- openmbv/openmbv/mainwindow.cc
+++ openmbv/openmbv/mainwindow.cc
@@ -39,6 +39,9 @@
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QColorDialog>
 #include <QtCore/QElapsedTimer>
+#include <QWebHistory>
+#include <QWebFrame>
+#include <QWebElement>
 #include <QShortcut>
 #include <QMimeData>
 #include <Inventor/nodes/SoBaseColor.h>
@@ -86,7 +89,7 @@ QObject* qTreeWidgetItemToQObject(const QModelIndex &index) {
   return dynamic_cast<QObject*>(static_cast<QTreeWidgetItem*>(index.internalPointer()));
 }
 
-MainWindow::MainWindow(list<string>& arg) :  fpsMax(25), enableFullScreen(false), deltaTime(0), oldSpeed(1) {
+MainWindow::MainWindow(list<string>& arg) :  fpsMax(25), helpViewerGUI(nullptr), helpViewerXML(nullptr), enableFullScreen(false), deltaTime(0), oldSpeed(1) {
   // If <local>/lib/dri exists use it as load path for GL DRI drivers.
   // DRI drivers depend on libstdc++.so. Hence, they must be distributed with the binary distribution.
   if(boost::filesystem::exists(MBXMLUtils::getInstallPath()/"lib"/"dri")) {
@@ -969,13 +972,22 @@ void MainWindow::help(const std::string& type, QDialog *helpDialog) {
     layout->addWidget(helpBackward,0,1);
     QPushButton *helpForward=new QPushButton("Forward",helpDialog);
     layout->addWidget(helpForward,0,2);
+    auto *helpViewer=new QWebView(helpDialog);
+    connect(helpViewer, SIGNAL(loadFinished(bool)), this, SLOT(loadFinished()));
+    layout->addWidget(helpViewer,1,0,1,3);
+    connect(helpForward, SIGNAL(clicked()), helpViewer, SLOT(forward()));
+    connect(helpBackward, SIGNAL(clicked()), helpViewer, SLOT(back()));
     if(type=="GUI") {
       helpDialog->setWindowTitle("OpenMBV - GUI Help");
       connect(home, SIGNAL(clicked()), this, SLOT(helpHomeGUI()));
+      helpViewer->load(QUrl((Utils::getDocPath()+"/guihelp.html").c_str()));
+      helpViewerGUI=helpViewer;
     }
     else if(type=="XML") {
       helpDialog->setWindowTitle("OpenMBV - XML Help");
       connect(home, SIGNAL(clicked()), this, SLOT(helpHomeXML()));
+      helpViewer->load(QUrl((Utils::getXMLDocPath()+"/http___www_mbsim-env_de_OpenMBV/index.html").c_str()));
+      helpViewerXML=helpViewer;
     }
   }
   helpDialog->show();
@@ -985,12 +997,25 @@ void MainWindow::help(const std::string& type, QDialog *helpDialog) {
 }
 
 void MainWindow::loadFinished() {
+  // set html fg color to gui fg color
+  if(helpViewerGUI)
+    helpViewerGUI->page()->mainFrame()->findFirstElement("body").setStyleProperty(
+      "color",
+      palette().text().color().name()
+    );
+  if(helpViewerXML)
+    helpViewerXML->page()->mainFrame()->findFirstElement("body").setStyleProperty(
+      "color",
+      palette().text().color().name()
+    );
 }
 
 void MainWindow::helpHomeGUI() {
+  helpViewerGUI->load(QUrl((Utils::getDocPath()+"/guihelp.html").c_str()));
 }
 
 void MainWindow::helpHomeXML() {
+  helpViewerXML->load(QUrl((Utils::getXMLDocPath()+"/http___www_mbsim-env_de_OpenMBV/index.html").c_str()));
 }
 
 void MainWindow::aboutOpenMBV() {
