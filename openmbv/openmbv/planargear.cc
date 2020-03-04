6a5c6ceb071dc1813a51adec2cc99daa7bf7ff75
Martin Förg <martin.o.foerg@gmail.com>
2020-03-04 13:42:53 +0100

    Enable 3D visualization of planar gears

--- openmbv/openmbv/planargear.cc
+++ openmbv/openmbv/planargear.cc
@@ -37,53 +37,99 @@ PlanarGear::PlanarGear(const std::shared_ptr<OpenMBV::Object> &obj, QTreeWidgetI
 
   // read XML
   int nz = e->getNumberOfTeeth();
-  double h = e->getHeight();
   double be = e->getHelixAngle();
   double al0 = e->getPressureAngle();
   double m = e->getModule()/cos(be);
   double b = e->getBacklash();
+  double w = e->getWidth();
+  double h = e->getHeight();
 
   double d0 = m*nz;
   double r0 = d0/2;
-  int numb = 10;
-  double du = e->getModule()/cos(al0)/numb;
-  vector<double> x(numb+1), y(numb+1), z(numb+1);
+  double dphi = (M_PI/2-b/m)/nz;
 
-  auto *cyl = new SoCylinder;
-  soSepRigidBody->addChild(cyl);
-  cyl->radius.setValue(m*nz/2);
-  cyl->height.setValue(h);
-
+  vector<double> x(8), y(8), z(8);
   for(int j=0; j<2; j++) {
     int signj=j?-1:1;
-    double u = -du/2;
-    for (int i=0; i<=numb; i++) {
-      x[i] = signj*u*sin(al0)*cos(be);
-      y[i] = u*cos(al0);
-      z[i] = signj*u*sin(al0)*sin(be)+r0;
-      u += du;
-    }
-    float pts[x.size()][3];
-    for(unsigned int i=0; i<x.size(); i++) {
-      pts[i][0] = x[i];
-      pts[i][1] = y[i];
-      pts[i][2] = z[i];
-    }
-    auto *r = new SoRotation;
-    soSepRigidBody->addChild(r);
-    r->rotation.setValue(SbVec3f(0,1,0),(j?2:-1)*(M_PI/2-b/m)/nz);
-    for(int k=0; k<nz; k++) {
-      auto *points = new SoCoordinate3;
-      auto *line = new SoLineSet;
-      points->point.setValues(0, x.size(), pts);
-      line->numVertices.setValue(x.size());
-      soSepRigidBody->addChild(points);
-      soSepRigidBody->addChild(line);
-      auto *r = new SoRotation;
-      soSepRigidBody->addChild(r);
-      r->rotation.setValue(SbVec3f(0,1,0),signj*2*M_PI/nz);
+    double eta = -e->getModule()/cos(al0);
+    double s = w/2;
+    double xi = (s-eta*sin(al0)*sin(be))/cos(be);
+    x[4*j+0] = -eta*sin(al0)*cos(be)+xi*sin(be);
+    y[4*j+0] = signj*eta*cos(al0);
+    z[4*j+0] = eta*sin(al0)*sin(be)+r0+xi*cos(be);
+    s = -w/2;
+    xi = (s-eta*sin(al0)*sin(be))/cos(be);
+    x[4*j+1] = -eta*sin(al0)*cos(be)+xi*sin(be);
+    y[4*j+1] = signj*eta*cos(al0);
+    z[4*j+1] = eta*sin(al0)*sin(be)+r0+xi*cos(be);
+    eta = e->getModule()/cos(al0);
+    s = -w/2;
+    xi = (s-eta*sin(al0)*sin(be))/cos(be);
+    x[4*j+2] = -eta*sin(al0)*cos(be)+xi*sin(be);
+    y[4*j+2] = signj*eta*cos(al0);
+    z[4*j+2] = eta*sin(al0)*sin(be)+r0+xi*cos(be);
+    s = w/2;
+    xi = (s-eta*sin(al0)*sin(be))/cos(be);
+    x[4*j+3] = -eta*sin(al0)*cos(be)+xi*sin(be);
+    y[4*j+3] = signj*eta*cos(al0);
+    z[4*j+3] = eta*sin(al0)*sin(be)+r0+xi*cos(be);
+    for(int i=4*j; i<4*j+4; i++) {
+      x[i] = cos(dphi)*x[i] + signj*sin(dphi)*z[i];
+      z[i] = -signj*sin(dphi)*x[i] + cos(dphi)*z[i];
     }
   }
+
+  int indices[25];
+  for(int i=0; i<4; i++)
+    indices[i] = i;
+  indices[4] = -1;
+  for(int i=5; i<9; i++)
+    indices[i] = i-1;
+  indices[9] = -1;
+  // top
+  indices[10] = 5;
+  indices[11] = 4;
+  indices[12] = 3;
+  indices[13] = 2;
+  indices[14] = -1;
+  // front
+  indices[15] = 0;
+  indices[16] = 3;
+  indices[17] = 4;
+  indices[18] = 7;
+  indices[19] = -1;
+  // back
+  indices[20] = 6;
+  indices[21] = 5;
+  indices[22] = 2;
+  indices[23] = 1;
+  indices[24] = -1;
+
+  float pts[x.size()][3];
+  for(unsigned int i=0; i<x.size(); i++) {
+    pts[i][0] = x[i];
+    pts[i][1] = y[i];
+    pts[i][2] = z[i];
+  }
+
+  for(int k=0; k<nz; k++) {
+    auto *points = new SoCoordinate3;
+    auto *face = new SoIndexedFaceSet;
+    points->point.setValues(0, x.size(), pts);
+    face->coordIndex.setValues(0, 25, indices);
+    soSepRigidBody->addChild(points);
+    soSepRigidBody->addChild(face);
+    auto *r = new SoRotation;
+    soSepRigidBody->addChild(r);
+    r->rotation.setValue(SbVec3f(0,1,0),2*M_PI/nz);
+  }
+  auto *t = new SoTranslation;
+  soSepRigidBody->addChild(t);
+  t->translation.setValue(SbVec3f(0,-e->getModule()-h/2,0));
+  auto *cyl = new SoCylinder;
+  soSepRigidBody->addChild(cyl);
+  cyl->radius.setValue(r0+w/2);
+  cyl->height.setValue(h);
 }
  
 void PlanarGear::createProperties() {
