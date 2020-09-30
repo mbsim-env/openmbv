729b09c7b601a4afedbfb6e1fdf7391cb9604819
Martin Förg <martin.o.foerg@gmail.com>
2020-09-30 15:27:30 +0200

    Enable outline for cylinders

--- openmbv/openmbv/cylinder.cc
+++ openmbv/openmbv/cylinder.cc
@@ -36,18 +36,77 @@ Cylinder::Cylinder(const std::shared_ptr<OpenMBV::Object> &obj, QTreeWidgetItem 
   iconFile="cylinder.svg";
   setIcon(0, Utils::QIconCached(iconFile));
 
-  // create so
-  auto *cylinder=new SoCylinder;
-  cylinder->radius.setValue(c->getRadius());
-  cylinder->height.setValue(c->getHeight());
-  soSepRigidBody->addChild(cylinder);
-  // scale ref/localFrame
   double size=min(c->getRadius(),c->getHeight())*c->getScaleFactor();
   refFrameScale->scaleFactor.setValue(size,size,size);
   localFrameScale->scaleFactor.setValue(size,size,size);
 
   // outline
+  const int num=128;
+  float radius = c->getRadius();
+  float y = c->getHeight()*0.5f;
+  float delta = (2.0*M_PI)/(double)num;
+  float angle = 0.0f;
+  float pts[2*num][3];
+  for (unsigned int i=0; i<num; i++) {
+    pts[i][0] = -sin(angle)*radius;
+    pts[i][1] = y;
+    pts[i][2] = -cos(angle)*radius;
+    pts[num+i][0] = pts[i][0];
+    pts[num+i][1] = -y;
+    pts[num+i][2] = pts[i][2];
+    angle += delta;
+  }
+  int index[2*num+4];
+  int k=0;
+  for(unsigned int i=0; i<num; i++)
+    index[k++] = i;
+  index[k++] = 0;
+  index[k++] = -1;
+  for(unsigned int i=num; i<2*num; i++)
+    index[k++] = i;
+  index[k++] = num;
+  index[k++] = -1;
+
+  int indexf[13*(num-1)+5];
+  k=0;
+  for(unsigned int i=0; i<num-1; i++) {
+    indexf[k++] = 0;
+    indexf[k++] = i;
+    indexf[k++] = i+1;
+    indexf[k++] = -1;
+  }
+  for(unsigned int i=num; i<2*num-1; i++) {
+    indexf[k++] = num;
+    indexf[k++] = i+1;
+    indexf[k++] = i;
+    indexf[k++] = -1;
+  }
+  for(unsigned int i=0; i<num-1; i++) {
+    indexf[k++] = num+i;
+    indexf[k++] = num+i+1;
+    indexf[k++] = i+1;
+    indexf[k++] = i;
+    indexf[k++] = -1;
+  }
+  indexf[k++] = 2*num-1;
+  indexf[k++] = num;
+  indexf[k++] = 0;
+  indexf[k++] = num-1;
+  indexf[k++] = -1;
+
+  auto *points = new SoCoordinate3;
+  points->point.setValues(0, 2*num, pts);
+
+  auto *line = new SoIndexedLineSet;
+  line->coordIndex.setValues(0, 2*num+4, index);
+
+  auto *face = new SoIndexedFaceSet;
+  face->coordIndex.setValues(0, 13*(num-1)+5, indexf);
+
+  soSepRigidBody->addChild(points);
+  soSepRigidBody->addChild(face);
   soSepRigidBody->addChild(soOutLineSwitch);
+  soOutLineSep->addChild(line);
 }
 
 void Cylinder::createProperties() {
