3190e1940ffc8cdaaf7f5a0c42d369d1af7e52ee
Martin Förg <martin.o.foerg@gmail.com>
2020-10-02 15:42:45 +0200

    Revise code

--- openmbv/openmbv/cylinder.cc
+++ openmbv/openmbv/cylinder.cc
@@ -56,52 +56,52 @@ Cylinder::Cylinder(const std::shared_ptr<OpenMBV::Object> &obj, QTreeWidgetItem 
     pts[num+i][2] = pts[i][2];
     angle += delta;
   }
-  int index[2*num+4];
+  int indl[2*num+4];
   int k=0;
   for(unsigned int i=0; i<num; i++)
-    index[k++] = i;
-  index[k++] = 0;
-  index[k++] = -1;
+    indl[k++] = i;
+  indl[k++] = 0;
+  indl[k++] = -1;
   for(unsigned int i=num; i<2*num; i++)
-    index[k++] = i;
-  index[k++] = num;
-  index[k++] = -1;
+    indl[k++] = i;
+  indl[k++] = num;
+  indl[k++] = -1;
 
-  int indexf[13*(num-1)+5];
+  int indf[13*(num-1)+5];
   k=0;
   for(unsigned int i=0; i<num-1; i++) {
-    indexf[k++] = 0;
-    indexf[k++] = i;
-    indexf[k++] = i+1;
-    indexf[k++] = -1;
+    indf[k++] = 0;
+    indf[k++] = i;
+    indf[k++] = i+1;
+    indf[k++] = -1;
   }
   for(unsigned int i=num; i<2*num-1; i++) {
-    indexf[k++] = num;
-    indexf[k++] = i+1;
-    indexf[k++] = i;
-    indexf[k++] = -1;
+    indf[k++] = num;
+    indf[k++] = i+1;
+    indf[k++] = i;
+    indf[k++] = -1;
   }
   for(unsigned int i=0; i<num-1; i++) {
-    indexf[k++] = num+i;
-    indexf[k++] = num+i+1;
-    indexf[k++] = i+1;
-    indexf[k++] = i;
-    indexf[k++] = -1;
+    indf[k++] = num+i;
+    indf[k++] = num+i+1;
+    indf[k++] = i+1;
+    indf[k++] = i;
+    indf[k++] = -1;
   }
-  indexf[k++] = 2*num-1;
-  indexf[k++] = num;
-  indexf[k++] = 0;
-  indexf[k++] = num-1;
-  indexf[k++] = -1;
+  indf[k++] = 2*num-1;
+  indf[k++] = num;
+  indf[k++] = 0;
+  indf[k++] = num-1;
+  indf[k++] = -1;
 
   auto *points = new SoCoordinate3;
   points->point.setValues(0, 2*num, pts);
 
   auto *line = new SoIndexedLineSet;
-  line->coordIndex.setValues(0, 2*num+4, index);
+  line->coordIndex.setValues(0, 2*num+4, indl);
 
   auto *face = new SoIndexedFaceSet;
-  face->coordIndex.setValues(0, 13*(num-1)+5, indexf);
+  face->coordIndex.setValues(0, 13*(num-1)+5, indf);
 
   soSepRigidBody->addChild(points);
   soSepRigidBody->addChild(face);
