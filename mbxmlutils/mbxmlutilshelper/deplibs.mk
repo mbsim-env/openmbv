2e636d16c9f65264f224770d6de4ce716cde0d80
Markus Friedrich <fm12111+github@googlemail.com>
2024-05-30 19:50:26 +0200

    Improved deplibs

--- mbxmlutils/mbxmlutilshelper/deplibs.mk
+++ mbxmlutils/mbxmlutilshelper/deplibs.mk
@@ -3,14 +3,19 @@ deplibs.target:
 	set -e; \
 	echo "Create dependency file(s) (only if python3 is found) for $(lib_LTLIBRARIES)"
 	if which python3 &> /dev/null; then \
+	  ANYFAILED=0; \
 	  for lib_la in $(lib_LTLIBRARIES); do \
 	    . $(libdir)/$$lib_la; \
 	    if test -n "$$dlname"; then \
 	      if test $$libdir/$$dlname -nt $$libdir/$$dlname.deplibs; then \
 	        echo "Create dependency files for $$libdir/$$dlname"; \
-	        python3 $(prefix)/share/mbxmlutils/python/deplibs.py $$libdir/$$dlname > $$libdir/$$dlname.deplibs.tmp; \
+	        python3 $(prefix)/share/mbxmlutils/python/deplibs.py $$libdir/$$dlname > $$libdir/$$dlname.deplibs.tmp || ANYFAILED=1; \
 	        mv -f $$libdir/$$dlname.deplibs.tmp $$libdir/$$dlname.deplibs; \
 	      fi; \
 	    fi; \
 	  done; \
+	  if test $$ANYFAILED = 1; then \
+	    echo "At least one deplibs command failed, see above."; \
+	    exit 1; \
+	  fi; \
 	fi
