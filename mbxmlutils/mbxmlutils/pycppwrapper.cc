959170c3c8bbf2b69ec2235a09255d271196b9d3
Markus Friedrich <fm12111+github@googlemail.com>
2024-09-07 17:21:20 +0200

    Fix

--- mbxmlutils/mbxmlutils/pycppwrapper.cc
+++ mbxmlutils/mbxmlutils/pycppwrapper.cc
@@ -123,10 +123,10 @@ void initializePython(const boost::filesystem::path &main, const std::string &py
   #endif
   Py_InitializeEx(0);
 
-  PyO os=CALLPY(PyImport_ImportModule, "os");
-  PyO os_add_dll_directory=CALLPY(PyObject_GetAttrString, os, "add_dll_directory");
-  auto appendToPATH = [&os_add_dll_directory, &pathsep](const std::string &path) {
+  auto appendToPATH = [&pathsep](const std::string &path) {
 #if defined(_WIN32) && PY_MAJOR_VERSION==3 && PY_MINOR_VERSION>=8
+    PyO os=CALLPY(PyImport_ImportModule, "os");
+    PyO os_add_dll_directory=CALLPY(PyObject_GetAttrString, os, "add_dll_directory");
     PyO arg(CALLPY(PyTuple_New, 1));
     PyO libdir(CALLPY(PyUnicode_FromString, path));
     CALLPY(PyTuple_SetItem, arg, 0, libdir.incRef());
