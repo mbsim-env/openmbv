83082f56fb01e609c86a0e602ac33c1a549dcabd
Markus Friedrich <friedrich@rcstengel.de>
2025-12-01 12:40:46 +0100

    Fixed test

--- mbxmlutils/mbxmlutils/python/testsymbolic.py
+++ mbxmlutils/mbxmlutils/python/testsymbolic.py
@@ -1,4 +1,4 @@
-import mbxmlutils
+import mbxmlutils._internal
 import sympy
 
 c=3.1
@@ -6,19 +6,19 @@ i=sympy.Dummy()
 s=2*i
 
 y=sympy.simplify(c+i+i*s)
-print(mbxmlutils._serializeFunction(y))
+print(mbxmlutils._internal._serializeFunction(y))
 
 y=sympy.atan2(c, s)
-print(mbxmlutils._serializeFunction(y))
+print(mbxmlutils._internal._serializeFunction(y))
 
 y=sympy.Heaviside(s)
-print(mbxmlutils._serializeFunction(y))
+print(mbxmlutils._internal._serializeFunction(y))
 
 y=sympy.Min(c, s, sympy.simplify(i*i))
-print(mbxmlutils._serializeFunction(y))
+print(mbxmlutils._internal._serializeFunction(y))
 
 y=sympy.Max(c, s, sympy.simplify(i*i))
-print(mbxmlutils._serializeFunction(y))
+print(mbxmlutils._internal._serializeFunction(y))
 
 y=sympy.Piecewise((s, sympy.simplify(i>1)), (c*c, sympy.simplify(s<2)), (s*i, sympy.simplify(2<i)))
-print(mbxmlutils._serializeFunction(y))
+print(mbxmlutils._internal._serializeFunction(y))
