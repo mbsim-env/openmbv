91883e8d59613e52c03359524b4aac8c09b3496b
Markus Friedrich <friedrich@rcstengel.de>
2026-01-13 17:19:34 +0100

    Make Cylinder smooth shaded

--- openmbv/openmbv/cylinder.cc
+++ openmbv/openmbv/cylinder.cc
@@ -19,8 +19,9 @@
 
 #include "config.h"
 #include "cylinder.h"
-#include <Inventor/nodes/SoCylinder.h>
 #include <Inventor/nodes/SoDrawStyle.h>
+#include <Inventor/nodes/SoNormal.h>
+#include <Inventor/nodes/SoShapeHints.h>
 #include <vector>
 #include "utils.h"
 #include "openmbvcppinterface/cylinder.h"
@@ -46,11 +47,18 @@ Cylinder::Cylinder(const std::shared_ptr<OpenMBV::Object> &obj, QTreeWidgetItem 
   float y = c->getHeight()*0.5f;
   float delta = (2.0*M_PI)/(double)num;
   float angle = 0.0f;
+  float nrm[2*num][3];
   float pts[2*num][3];
   for (unsigned int i=0; i<num; i++) {
-    pts[i][0] = -sin(angle)*radius;
+    nrm[i][0] = -sin(angle);
+    nrm[i][1] = 0;
+    nrm[i][2] = -cos(angle);
+    nrm[num+i][0] = nrm[i][0];
+    nrm[num+i][1] = 0;
+    nrm[num+i][2] = nrm[i][2];
+    pts[i][0] = nrm[i][0]*radius;
     pts[i][1] = y;
-    pts[i][2] = -cos(angle)*radius;
+    pts[i][2] = nrm[i][2]*radius;
     pts[num+i][0] = pts[i][0];
     pts[num+i][1] = -y;
     pts[num+i][2] = pts[i][2];
@@ -67,20 +75,8 @@ Cylinder::Cylinder(const std::shared_ptr<OpenMBV::Object> &obj, QTreeWidgetItem 
   indl[k++] = num;
   indl[k++] = -1;
 
-  int indf[13*(num-1)+5];
+  int indf[5*(num-1)+5];
   k=0;
-  for(unsigned int i=0; i<num-1; i++) {
-    indf[k++] = 0;
-    indf[k++] = i;
-    indf[k++] = i+1;
-    indf[k++] = -1;
-  }
-  for(unsigned int i=num; i<2*num-1; i++) {
-    indf[k++] = num;
-    indf[k++] = i+1;
-    indf[k++] = i;
-    indf[k++] = -1;
-  }
   for(unsigned int i=0; i<num-1; i++) {
     indf[k++] = num+i;
     indf[k++] = num+i+1;
@@ -97,14 +93,38 @@ Cylinder::Cylinder(const std::shared_ptr<OpenMBV::Object> &obj, QTreeWidgetItem 
   auto *points = new SoCoordinate3;
   points->point.setValues(0, 2*num, pts);
 
+  auto *normals = new SoNormal;
+  normals->vector.setValues(0, 2*num, nrm);
+
   auto *line = new SoIndexedLineSet;
   line->coordIndex.setValues(0, 2*num+4, indl);
 
   auto *face = new SoIndexedFaceSet;
-  face->coordIndex.setValues(0, 13*(num-1)+5, indf);
+  face->coordIndex.setValues(0, 5*(num-1)+5, indf);
 
   soSepRigidBody->addChild(points);
+  soSepRigidBody->addChild(normals);
   soSepRigidBody->addChild(face);
+
+  auto *nb = new SoNormalBinding;
+  soSepRigidBody->addChild(nb);
+  nb->value = SoNormalBinding::PER_FACE;
+  for(int type : { 0, 1 }) {
+    auto *sh = new SoShapeHints;
+    soSepRigidBody->addChild(sh);
+    sh->vertexOrdering = type==0?SoShapeHints::COUNTERCLOCKWISE:SoShapeHints::CLOCKWISE;
+    auto *normal = new SoNormal;
+    soSepRigidBody->addChild(normal);
+    normal->vector.set1Value(0, 0,type==0?1:-1,0);
+    auto *cap = new SoIndexedFaceSet;
+    soSepRigidBody->addChild(cap);
+    cap->coordIndex.setNum(num);
+    auto idx=cap->coordIndex.startEditing();
+    for(int i=0; i<num; ++i)
+      idx[i] = i + (type==0?0:num);
+    cap->coordIndex.finishEditing();
+  }
+
   soSepRigidBody->addChild(soOutLineSwitch);
   soOutLineSep->addChild(line);
 }
