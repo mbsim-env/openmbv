9111fa4ec05b78930028ba8035c1b399db06ea9f
Markus Friedrich <fm12111+github@googlemail.com>
2026-03-22 00:36:32 +0100

    Fix

--- mbxmlutils/mbxmlutilshelper/prettyprint.cc
+++ mbxmlutils/mbxmlutilshelper/prettyprint.cc
@@ -1,5 +1,6 @@
 #include <boost/algorithm/string/predicate.hpp>
 #include <boost/algorithm/string/split.hpp>
+#include <boost/filesystem/fstream.hpp>
 #ifdef _WIN32
 #  define WIN32_LEAN_AND_MEAN
 #  include <windows.h>
